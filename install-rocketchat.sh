6533730dc7cacb327c487b9632297b2b4e5ee1bb
Justin <justin.zy.wang@gmail.com>
2017-12-12 10:03:18 +0800

    added hubot

--- install-rocketchat.sh
+++ install-rocketchat.sh
@@ -9,7 +9,7 @@ currentTime()
   date +"%Y-%m-%d %H:%M:%S";
 }
 
-
+sudo docker service scale devops-rocketchathubot=0
 sudo docker service scale devops-rocketchat=0
 sudo docker service scale devops-rocketchatdb=0
 
@@ -40,5 +40,21 @@ volume-driver=local-persist,volume-opt=mountpoint=/var/nfs/volumes/devops_rocket
 --constraint 'node.role == manager' \
 $ROCKETCHAT_IMAGE
 
+echo ---$(currentTime)---create hubot service---
+sudo docker service create -d \
+--name devops-rocketchathubot \
+--mount type=volume,source=devops_rocketchathubot_volume,destination=/home/hubot/scripts,\
+volume-driver=local-persist,volume-opt=mountpoint=/var/nfs/volumes/devops_rocketchathubot_volume \
+--mount type=bind,source=/var/run/docker.sock,destination=/var/run/docker.sock \
+--mount type=bind,source=/usr/bin/docker,destination=/usr/bin/docker \
+--network $NETWORK_NAME \
+--replicas 1 \
+--constraint 'node.role == manager' \
+$ROCKETCHATHUBOT_IMAGE
+
+
+
+
 sudo docker service scale devops-rocketchatdb=1
 sudo docker service scale devops-rocketchat=1
+sudo docker service scale devops-rocketchathubot=1
